6275b1031558652ec402cb77c569c58626ad9b41
Kendall Willets <kendall@willets.org>
2017-10-07 09:02:10 -0700

    encoding works not integrated yet

--- src/encode_poc.c
+++ src/encode_poc.c
@@ -1,6 +1,7 @@
 
 #include <x86intrin.h>
 #include <stdio.h>
+#include "shuffle_tables.h"
 
 // POC standalone test until code looks ok
 // usage:  gcc -msse4.1 -o encode_poc encode_poc.c
@@ -32,12 +33,12 @@ const u128 Ones = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
 #define shifter (1|1<<9|1<<18)
 const u128 Shifts = {.u32 = {shifter,shifter,shifter,shifter}};
 
-// translate 3-bitmaps into lane codes
-const u128 Codes   = {0,3,2,3,1,3,2,3,0,0,0,0,0,0,0,0};
+// translate 3-bit maps into lane codes
+const u128 LaneCodes   = {0,3,2,3,1,3,2,3,0,0,0,0,0,0,0,0};
 
 const u128 CollectHi = {15,11,7,3,15,11,7,3,0,0,0,0,0,0,0,0};
 
-// mul-shift 2-bit codes into high byte
+// mul-shift 2-bit lane codes into high byte
 #define concat (1|1<<10|1<<20|1<<30)
 #define sum    (1|1<<8|1<<16|1<<24)
 const u128 Aggregates = {.u32 = {concat, sum, 0, 0}};
@@ -46,9 +47,9 @@ size_t streamvbyte_encode(u128 *in, uint8_t *outData, uint8_t *outCode) {
 
   __m128i mins = _mm_min_epu8(in->i128, Ones.i128);
   __m128i bytemaps = _mm_mullo_epi32( mins, Shifts.i128);
-  __m128i lanecodes = _mm_shuffle_epi8(Codes.i128, bytemaps);
+  __m128i lanecodes = _mm_shuffle_epi8(LaneCodes.i128, bytemaps);
 
-  // pshufb high bytes into one lane
+  // pshufb high bytes into lanes 0,1
   __m128i hibytes = _mm_shuffle_epi8(lanecodes, CollectHi.i128);
 
   u128 codeAndLength = {.i128 = _mm_mullo_epi32(hibytes, Aggregates.i128)};
@@ -56,18 +57,23 @@ size_t streamvbyte_encode(u128 *in, uint8_t *outData, uint8_t *outCode) {
   size_t length = codeAndLength.i8[7] + 4;
 
   // get shuffle[code]
+  __m128i Shuf = *(__m128i *) &encodingShuffleTable[code];
 
-  // shuffle (1)
-  // store bytes (1)
-  return length; /// size
+  *(__m128i *) outData = _mm_shuffle_epi8(in->i128, Shuf);
+
+  *outCode = code;
+  return length;
 }
 
 int main() {
-  uint32_t data[] = { 10, 1000, 100000, 100000000 };
+  uint32_t data[] = { 0xaa, 0xbbb, 0xcccccc, 0xdddddddd };
   uint8_t outData[16], outCode[1];
+  int x;
 
   size_t len = streamvbyte_encode((u128 *) data, outData, outCode);
   printf("code 0x%02x\n", outCode[0]);
   printf("length %d\n", (int) len);
-
+  for(int i =0; i < len; i++)
+    printf(" %02x", outData[i] & 0xFF);
+  printf("\n");
 }
