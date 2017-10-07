d7f092c8efe51e97bebd57925909773e4819be35
Kendall Willets <kendall@willets.org>
2017-10-07 11:47:57 -0700

    clean up

--- src/encode_poc.c
+++ src/encode_poc.c
@@ -36,10 +36,13 @@ const u128 Shifts = {.u32 = {shifter,shifter,shifter,shifter}};
 // translate 3-bit maps into lane codes
 const u128 LaneCodes   = {0,3,2,3,1,3,2,3,0,0,0,0,0,0,0,0};
 
-const u128 CollectHi = {15,11,7,3,15,11,7,3,0,0,0,0,0,0,0,0};
+// gather high bytes from each lane, 2 copies
+const u128 GatherHi = {15,11,7,3,15,11,7,3,0,0,0,0,0,0,0,0};
 
-// mul-shift 2-bit lane codes into high byte
+// mul-shift magic numbers
+// concatenate 2-bit lane codes into high byte
 #define concat (1|1<<10|1<<20|1<<30)
+// sum lane codes in high byte
 #define sum    (1|1<<8|1<<16|1<<24)
 const u128 Aggregators = {.u32 = {concat, sum, 0, 0}};
 
@@ -48,15 +51,13 @@ size_t streamvbyte_encode4(u128 *in, uint8_t *outData, uint8_t *outCode) {
   __m128i mins      = _mm_min_epu8(in->i128, Ones.i128);
   __m128i bytemaps  = _mm_mullo_epi32( mins, Shifts.i128);
   __m128i lanecodes = _mm_shuffle_epi8(LaneCodes.i128, bytemaps);
-
-  __m128i hibytes   = _mm_shuffle_epi8(lanecodes, CollectHi.i128);
+  __m128i hibytes   = _mm_shuffle_epi8(lanecodes, GatherHi.i128);
 
   u128 codeAndLength = {.i128 = _mm_mullo_epi32(hibytes, Aggregators.i128)};
   uint8_t code  = codeAndLength.i8[3];
   size_t length = codeAndLength.i8[7] + 4;
 
   __m128i Shuf = *(__m128i *) &encodingShuffleTable[code];
-
   __m128i outAligned = _mm_shuffle_epi8(in->i128, Shuf);
 
   _mm_storeu_si128 ((__m128i *) outData, outAligned);
@@ -67,22 +68,23 @@ size_t streamvbyte_encode4(u128 *in, uint8_t *outData, uint8_t *outCode) {
 static uint8_t *svb_encode_vector(const uint32_t *in,
 		uint8_t *__restrict__ keyPtr, uint8_t *__restrict__ dataPtr,
 		uint32_t count) {
-  if (count == 0)
-    return dataPtr; // exit immediately if no data                                          
 
   uint8_t * outData = dataPtr;
-  uint8_t * outKey = keyPtr;
+  uint8_t * outKey  = keyPtr;
 
-  for (uint32_t c = 0; c <= count-4; c += 4)
+  for (uint32_t c = 0; c <= count - 4; c += 4)
     outData += streamvbyte_encode4((u128 *) (in + c), outData, outKey++);
 
-  // todo: fixup last few
+  // todo:  integrate and make this valid:
+  //  outData = svb_encode_scalar(in + c, outKey, outData, count - c);
+
   return outData;
 }
 
+// ugly test code
 int main() {
 
-  uint32_t data[] = { 0xfeed, 0xbbb, 0xcccccc, 0xdeadbeef, 0,0,0,0 };
+  uint32_t data[] = { 0xfeed, 0xbbb, 0xcccccc, 0xdeadbeef, 0,0,0,0, 0x1337, 0xdadfeed, 0, 0x1234 };
   uint8_t outData[100], outCode[10];
 
   size_t len = streamvbyte_encode4((u128 *) data, outData, outCode);
@@ -92,7 +94,7 @@ int main() {
     printf(" %02x", outData[i] & 0xFF);
   printf("\n");
 
-  uint8_t * out = svb_encode_vector( data, outCode, outData, 8 );
+  uint8_t * out = svb_encode_vector( data, outCode, outData, 12 );
   len = out - outData;
   printf("length %d\n", (int) len);
   for(int i = 0; i < len; i++)
