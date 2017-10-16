8d4da925f0283f4b193c7cfc3dd9680370db74e7
Kendall Willets <kendall@willets.org>
2017-10-16 06:01:33 +0000

    added output shuffle

--- src/neon_poc.c
+++ src/neon_poc.c
@@ -5,6 +5,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "streamvbyte_shuffle_tables.h"
+
 int main() {
 
 uint32x4_t mask = {3,3,3,3};
@@ -16,17 +18,19 @@ uint8x8_t gatherlo = {12, 8, 4, 0, 12, 8, 4, 0};
 #define sum (1 | 1 << 8 | 1 << 16 | 1 << 24)
 uint32x2_t Aggregators = {concat, sum};
 
-uint32x4_t data = {1,256,100000, 1024};
+uint32x4_t data = {1, 0xFF, 0xdeadbeef, 0xab };
 
+
+// lane code is 3 ^ (clz(data)/8)
 uint32x4_t b = vshrq_n_u32(vclzq_u32(data), 3);
-
 uint32x4_t lanecodes = veorq_u32(b, mask);
 
+// nops
 uint8x16_t lanebytes = vreinterpretq_u8_u32(lanecodes);
-uint8x8x2_t halves = {vget_low_u8(lanebytes), vget_high_u8(lanebytes)};
+uint8x8x2_t twohalves = {vget_low_u8(lanebytes), vget_high_u8(lanebytes)};
 
 // shuffle msbytes into two copies of an int
-uint8x8_t lobytes = vtbl2_u8( halves, gatherlo);
+uint8x8_t lobytes = vtbl2_u8( twohalves, gatherlo);
 
 uint32x2_t mulshift = vreinterpret_u32_u8(lobytes);
 
@@ -34,6 +38,24 @@ uint32x2_t codeAndLength = vmul_u32(mulshift, Aggregators);
 
 uint32_t code = codeAndLength[0]>>24;
 size_t length = 4+(codeAndLength[1]>>24) ;
+
+// shuffle in 8-byte chunks
+uint8x16_t databytes = vreinterpretq_u8_u32(data);
+uint8x8x2_t datahalves = {vget_low_u8(databytes), vget_high_u8(databytes)};
+
+uint8x8_t first8 = *(uint8x8_t *) &encodingShuffleTable[code];
+uint8x8_t out = vtbl2_u8(datahalves, first8);
+
+if( length > 8 ) {
+	uint8x8_t last8 = *(uint8x8_t *) (8 + (void*)&encodingShuffleTable[code]);
+	uint8x8_t out2 = vtbl2_u8(datahalves, last8);
+}
+for( int i = 0; i < 8; i++)
+	printf("%2x ", out[i]);
+printf("\n");
+// shuffle(first 8 of mask)
+// if( length > 8 )
+//  shuffle(2nd half of mask)
 
 for( int i =0; i < 4; i++)
 	printf("lane code=%d\n", lanecodes[i] );
