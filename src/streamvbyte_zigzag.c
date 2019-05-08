92ab8a74c96b9e0f684c45aec9cf7f57405ff8da
Chris Seymour <chris.j.seymour@hotmail.com>
2019-05-08 17:18:38 +0100

    #28 add delta zigzag (#29)

--- src/streamvbyte_zigzag.c
+++ src/streamvbyte_zigzag.c
@@ -10,6 +10,12 @@ void zigzag_encode(const int32_t * in, uint32_t * out, size_t N) {
       out[i] = _zigzag_encode_32(in[i]);
 }
 
+void zigzag_delta_encode(const int32_t * in, uint32_t * out, size_t N, int32_t prev) {
+    for (size_t i = 0; i < N; i++) {
+      out[i] = _zigzag_encode_32(in[i] - prev);
+      prev = in[i];
+    }
+}
 
 static inline
 int32_t _zigzag_decode_32 (uint32_t val) {
@@ -20,4 +26,13 @@ int32_t _zigzag_decode_32 (uint32_t val) {
 void zigzag_decode(const uint32_t * in, int32_t * out, size_t N) {
     for(size_t i = 0; i < N; i++)
       out[i] = _zigzag_decode_32(in[i]);
+}
+
+
+void zigzag_delta_decode(const uint32_t * in, int32_t * out, size_t N, int32_t prev) {
+    for(size_t i = 0; i < N; i++) {
+      int32_t val =_zigzag_decode_32(in[i]);
+      out[i] = val + prev;
+      prev += val;
+    }
 }
