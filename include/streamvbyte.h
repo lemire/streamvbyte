85ca791977bbc9db5bbea86633b387f4a9d830cc
daniel-j-h <daniel@trvx.org>
2021-07-04 21:44:27 +0200

    Adds optional two-pass function to estimate required memory, closes #32

--- include/streamvbyte.h
+++ include/streamvbyte.h
@@ -26,6 +26,8 @@ size_t streamvbyte_encode(const uint32_t *in, uint32_t length, uint8_t *out);
 size_t streamvbyte_encode_0124(const uint32_t *in, uint32_t length, uint8_t *out);
 
 // return the maximum number of compressed bytes given length input integers
+// in the worst case we overestimate data bytes required by four, see below
+// for a function you can run upfront over your data to compute allocations
 static inline size_t streamvbyte_max_compressedbytes(const uint32_t length) {
    // number of control bytes:
    size_t cb = (length + 3) / 4;
@@ -34,7 +36,24 @@ static inline size_t streamvbyte_max_compressedbytes(const uint32_t length) {
    return cb + db;
 }
 
+// return the exact number of compressed bytes given length input integers
+// runtime in O(n) wrt. in; use streamvbyte_max_compressedbyte if you
+// care about speed more than potentially over-allocating memory
+static inline size_t streamvbyte_compressedbytes(const uint32_t *in, uint32_t length) {
+   // number of control bytes:
+   size_t cb = (length + 3) / 4;
+   // maximum number of control bytes:
+   size_t db = 0;
+   for (uint32_t c = 0; c < length; c++) {
+      uint32_t val = in[c];
 
+      if (val < (1 << 8)) db += 1;
+      else if (val < (1 << 16)) db += 2;
+      else if (val < (1 << 24)) db += 3;
+      else db += 4;
+   }
+   return cb + db;
+}
 
 
 // Read "length" 32-bit integers in varint format from in, storing the result in out.
