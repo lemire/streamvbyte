a7d84aa4673a8992c4be5d2c18b188a146a50dac
Daniel Lemire <lemire@gmail.com>
2021-07-05 13:04:30 -0400

    Adding undef. tests. (#36)

--- src/streamvbyte_zigzag.c
+++ src/streamvbyte_zigzag.c
@@ -1,8 +1,8 @@
 #include "streamvbyte_zigzag.h"
 
 static inline
-uint32_t _zigzag_encode_32 (int32_t val) {
-	return (val + val) ^ (val >> 31);
+uint32_t _zigzag_encode_32 (uint32_t val) {
+	return (val + val) ^ ((int32_t)val >> 31);
 }
 
 void zigzag_encode(const int32_t * in, uint32_t * out, size_t N) {
