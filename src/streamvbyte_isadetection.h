65f7af602c0afff3c4b79e5c11988f8acf808b59
Aras Pranckevičius <aras@nesnausk.org>
2023-02-03 16:16:03 +0200

    Fix build on clang-cl 15.0 (#50)
    For me at least in VS2022 16.4, when using clang-cl (15.0), it does not build since some SSE3 or SSE4 intrinsics are being compiled, without including the headers that define them.

--- src/streamvbyte_isadetection.h
+++ src/streamvbyte_isadetection.h
@@ -47,6 +47,8 @@ POSSIBILITY OF SUCH DAMAGE.
 #if defined(_MSC_VER)
 /* Microsoft C/C++-compatible compiler */
 #include <intrin.h>
+#include <tmmintrin.h>
+#include <smmintrin.h>
 #elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
 /* GCC-compatible compiler, targeting x86/x86-64 */
 #include <x86intrin.h>
